f4ed423cecbcd39aa859d589ccaa50c7944a8dce
tpoornashree <TPOORNASHREE@GMAIL.COM>
2022-10-02 17:09:05 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '3619717463msh56f10022631af9dp18962djsn902720ac8b98',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '3619717463msh56f10022631af9dp18962djsn902720ac8b98',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
